7af4c95e5e745aa37e03b3f606c30e2b08514fb2
Maria Hensley <mscelie34@zoho.com>
2023-02-04 01:38:29 -0600

    methodover

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace MethodOverloading
+﻿
+using System.ComponentModel;
+
+namespace MethodOverloading
 {
     public class Program
     {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static decimal Add(decimal decimal1, decimal decimal2)
+        {
+            return decimal1 + decimal2;
+        }
+
+        public static string Add(int num1, int num2, bool isTrue)
+        {
+            var sum = 0;
+            var response = "";
+            if(isTrue)
+            {
+                sum = num1 + num2;
+
+                if (sum == 1)
+                    return $"{sum} dollar.";
+                else
+                {
+                    return $"{sum} dollars.";
+                }
+            }
+            return response;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(5, -4, true));
+            Console.WriteLine();
+            Console.WriteLine(Add(20, 3, true));
+            Console.WriteLine();
+            Console.WriteLine(Add(-16, 4, true));
         }
     }
 }
